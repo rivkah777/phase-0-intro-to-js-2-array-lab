259f9f1856feaa0b617dc7e1ca8c527bcbffcd00
Rebekah <rbk1008@Gmail.com>
2021-10-24 11:24:35 -0600

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution here!
+const cats = ['Milo','Otis','Garfield'];
+  
+  function destructivelyAppendCat(name) {
+    cats.push(name);
+
+  }
+  
+ function destructivelyPrependCat(name) {
+   cats.unshift(name);
+
+ }
+  
+  function destructivelyRemoveLastCat() {
+    cats.splice(-1);
+  }
+
+  function destructivelyRemoveFirstCat() {
+    cats.splice(0,1);
+  }
+
+  
+
+ 
+  function appendCat(name) {
+    let newCats = [...cats,name];
+    return newCats;
+  }
+ 
+
+ 
+  function prependCat(name) {
+    let orangeCat = [name, ...cats];
+    return orangeCat;
+
+  }
+  function  removeLastCat() {
+    let purpleCats = [...cats];
+    purpleCats.splice(-1)
+    return purpleCats;
+  } 
+
+  
+ function removeFirstCat() {
+   let pinkCats = [...cats];
+   pinkCats.splice(0,1)
+   return pinkCats
+ }
+  
+  
+  
+  
